3285c75320152548cbbd559ff94fde04e71929ee
Hoai_Pc <machhoai1234@gmail.com>
2023-06-29 07:46:03 +0700

    Top nav

--- my-project/src/views/Top.vue
+++ my-project/src/views/Top.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="top">
-    <h1>top</h1>
-  </div>
+    <ContentTop/>
 </template>
+
+<script>
+// @ is an alias to /src
+import ContentTop from '@/components/ContentOfTop.vue'
+export default {
+  name: 'TopView',
+  components: {
+    ContentTop
+    
+  }
+}
+</script>
+
+<style>
+  .home
+  {
+    width: 100%;
+  }
+</style>
